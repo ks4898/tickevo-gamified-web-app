94f8829d14e0496470df27e48b6eadd85aa117a6
Karlo Skokic <ks4898@g.rit.edu>
2025-03-24 17:20:34 +0100

    sum

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const bcrypt = require('bcrypt');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
